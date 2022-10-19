054fd3adba1177767f582394c0e3a4fac5a35aaf
Nick <nickcorreagarcia@gmail.com>
2022-10-19 13:34:11 -0500

    add fecth in app component with conexion proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <h1>App in React</h1>
+      {typeof backendData.users === "undefined" ? (
+        <p> Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user} </p>)
+      )}
     </div>
   );
 }
