a13bd407d0dfbd1bb7d1edcb5753352e0600bd8b
Nick <nickcorreagarcia@gmail.com>
2022-10-20 16:28:04 -0500

    update app.js add new route HomeRestaurant and remove App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,13 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import RestaurantsServices from "./services/restaurants_services";
-import Filter from "./components/filter/Filter";
-import Header from "./components/header/Header";
-import Section from "./components/section/Section";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeRestaurant from "./screens/HomeRestaurants/HomeRestaurants";
 
 function App() {
-  const [restaurants, setRestaurants] = useState([]);
-
-  useEffect(() => {
-    const restaurantService = new RestaurantsServices();
-    restaurantService.show().then((data) => {
-      setRestaurants(data);
-    });
-  }, []);
-
-  const handleSearch = async (e) => {
-    const newRestaurant = e.target.value;
-    const restaurantService = new RestaurantsServices();
-
-    if (newRestaurant) {
-      restaurantService.show(newRestaurant).then((data) => {
-        setRestaurants(data);
-      });
-    }
-  };
-
   return (
-    <div className="container">
-      <Header />
-      <Filter placeholder={"Busca tu restaurante"} onChange={handleSearch} />
-      <Section restaurants={restaurants} />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomeRestaurant />} />
+      </Routes>
+    </Router>
   );
 }
 
