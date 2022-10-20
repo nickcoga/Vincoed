6f405e593b9387022772de69f6ec650d5e9e967b
Nick <nickcorreagarcia@gmail.com>
2022-10-19 19:38:04 -0500

    update app.js in server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,12 @@
 import express from "express";
+import restaurantsRoutes from "./routes/restaurants.routes.js";
+import dishesRoutes from "./routes/dishes.routes.js";
 
 const app = express();
+
+//midelwares
+app.use(express.json());
+app.use(restaurantsRoutes);
+app.use(dishesRoutes);
 
 export default app;
