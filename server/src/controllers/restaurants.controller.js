ad7ebd4e56dbf3b6cbf9d4d073e90465b7b8d976
Nick <nickcorreagarcia@gmail.com>
2022-10-19 21:51:22 -0500

    update controllers restaurant with filter with query param

--- server/src/controllers/restaurants.controller.js
+++ server/src/controllers/restaurants.controller.js
@@ -1,11 +1,26 @@
 import { Restaurant } from "../models/Restaurant.js";
 import { Op } from "sequelize";
+import { sequelize } from "../database/database.js";
 import { Dish } from "../models/Dish.js";
 
 export const getRestaurants = async (req, res) => {
+  const { name } = req.query;
   try {
-    const restaurant = await Restaurant.findAll();
-    res.json(restaurant);
+    if (name) {
+      const restaurants = await Restaurant.findAll({
+        where: {
+          name: sequelize.where(
+            sequelize.fn("LOWER", sequelize.col("name")),
+            "LIKE",
+            "%" + name.toLowerCase() + "%"
+          ),
+        },
+      });
+      res.json(restaurants);
+    } else {
+      const restaurants = await Restaurant.findAll();
+      res.json(restaurants);
+    }
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -15,7 +30,7 @@ export const getRestaurantName = async (req, res) => {
   const { name } = req.params;
 
   try {
-    const restaurant = await Restaurant.findAll({
+    const restaurants = await Restaurant.findAll({
       where: {
         name: {
           [Op.like]: "%" + name + "%",
@@ -23,10 +38,10 @@ export const getRestaurantName = async (req, res) => {
       },
     });
 
-    if (!restaurant || [])
+    if (restaurants.length === 0)
       return res.status(404).json({ message: "Restaurant does no exists" });
 
-    res.json(restaurant);
+    res.json(restaurants);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
