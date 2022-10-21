27d7ddd88b3a0ed6d0f57d57328055d1c46c555a
Nick <nickcorreagarcia@gmail.com>
2022-10-20 23:57:48 -0500

    update restaurant services with CRUD

--- client/src/services/restaurants_services.js
+++ client/src/services/restaurants_services.js
@@ -15,4 +15,43 @@ RestaurantsServices.prototype.show = (name) => {
   }
 };
 
+RestaurantsServices.prototype.update = (id, name, city, address, image) => {
+  return fetch(`/restaurants/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      name,
+      city,
+      address,
+      image,
+    }),
+  });
+};
+
+RestaurantsServices.prototype.create = (name, city, address, image) => {
+  return fetch(`/restaurants`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      name,
+      city,
+      address,
+      image,
+    }),
+  });
+};
+
+RestaurantsServices.prototype.delete = (id) => {
+  return fetch(`restaurants/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  });
+};
+
 export default RestaurantsServices;
