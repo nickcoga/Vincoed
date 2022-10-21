c3fcb4538547ccb72229a0a43a56d47012eb1b97
Nick <nickcorreagarcia@gmail.com>
2022-10-20 23:44:43 -0500

    update styles of card component and add icons for CRUD

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -1,12 +1,38 @@
 import "./Card.css";
-export default function Card({ name, city, address, image }) {
+export default function Card({
+  name,
+  city,
+  address,
+  image,
+  onRestaurantClick,
+  onEditRestaurantClick,
+  onDeleteRestaurantClick,
+}) {
   return (
     <div className="content__card">
-      <img src={image} alt="" />
+      <img
+        src={
+          image | (image.length > 25)
+            ? image
+            : "https://www.ecpgr.cgiar.org/fileadmin/templates/ecpgr.org/Assets/images/No_Image_Available.jpg"
+        }
+        alt="No_image_avaible"
+      />
       <div className="content__text">
         <h3 className="content__title">{name}</h3>
-        <div>Ciudad: {city}</div>
-        <div>Dirección: {address}</div>
+        <p>Ciudad: {city}</p>
+        <p>Dirección: {address}</p>
+      </div>
+      <div className="content__icons">
+        <i
+          className="fa-utensils fas fa-utensils"
+          onClick={onRestaurantClick}
+        ></i>
+        <i className="fa-edit far fa-edit" onClick={onEditRestaurantClick}></i>
+        <i
+          className="fa-trash fas fa-trash"
+          onClick={onDeleteRestaurantClick}
+        ></i>
       </div>
     </div>
   );
