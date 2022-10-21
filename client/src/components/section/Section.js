edb5a9b946cf1727c49fa292e409b2650ea10805
Nick <nickcorreagarcia@gmail.com>
2022-10-20 23:48:17 -0500

    update section component

--- client/src/components/section/Section.js
+++ client/src/components/section/Section.js
@@ -1,8 +1,29 @@
 import "./Section.css";
 import "../card/Card";
 import Card from "../card/Card";
+import { useNavigate } from "react-router-dom";
+import RestaurantsServices from "../../services/restaurants_services";
 
 export default function Section({ restaurants }) {
+  let navigate = useNavigate();
+
+  const handleCardRestaurantDishes = (id) => {
+    navigate(`/restaurants/${id}`);
+  };
+
+  const handleEditResturant = (id) => {
+    navigate(`/restaurants/${id}/edit`);
+  };
+
+  const handleDeleteCard = (id) => {
+    const restaunrantService = new RestaurantsServices();
+
+    try {
+      restaunrantService.delete(id);
+      navigate(0);
+    } catch (error) {}
+  };
+
   return (
     <section className="content__section">
       {!restaurants ? (
@@ -15,6 +36,9 @@ export default function Section({ restaurants }) {
             city={restaurant.city}
             address={restaurant.address}
             image={restaurant.image}
+            onEditRestaurantClick={() => handleEditResturant(restaurant.id)}
+            onRestaurantClick={() => handleCardRestaurantDishes(restaurant.id)}
+            onDeleteRestaurantClick={() => handleDeleteCard(restaurant.id)}
           />
         ))
       )}
