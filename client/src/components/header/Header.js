d17781adbb704fe4cf220c711d9300e577ba937b
Nick <nickcorreagarcia@gmail.com>
2022-10-20 23:43:18 -0500

    update styles of header component

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,26 @@
 import "./Header.css";
 import logo from "../../assets/logo-restaurant.jpg";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  let navigate = useNavigate();
+
+  const handleCreateRestaurant = () => {
+    navigate("/restaurants/new");
+  };
+
   return (
     <header>
       <div className="content">
         <img src={logo} alt="logo.jpg" />
+      </div>
+      <div className="content__iconNew">
+        <i
+          className="fa-plus-circle fas fa-plus-circle"
+          onClick={handleCreateRestaurant}
+        >
+          Crear restaurant
+        </i>
       </div>
     </header>
   );
