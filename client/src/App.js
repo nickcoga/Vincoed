9fccba9dcf164e1618d3a72b14a483d66faf600f
Nick <nickcorreagarcia@gmail.com>
2022-10-21 19:29:39 -0500

    Add new routes in app.js client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateDishes from "./screens/CreateDishes/CreateDishes";
 import CreateRestaurant from "./screens/CreateRestaurant/CreateRestaurant";
 import Dishes from "./screens/Dishes/Dishes";
+import EditDishes from "./screens/EditDishes/EditDIshes";
 import EditRestaurant from "./screens/EditRestaurant/EditRestaurant";
 import HomeRestaurant from "./screens/HomeRestaurants/HomeRestaurants";
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
+  const [dishes, setDishes] = useState([]);
 
   return (
     <Router>
@@ -26,7 +29,15 @@ function App() {
           path="/restaurants/:id/edit"
           element={<EditRestaurant restaurants={restaurants} />}
         />
-        <Route path="/restaurants/:id" element={<Dishes />} />
+        <Route
+          path="/restaurants/:id"
+          element={<Dishes dishes={dishes} setDishes={setDishes} />}
+        />
+        <Route path="/restaurants/:id/dishes/new" element={<CreateDishes />} />
+        <Route
+          path="/restaurants/:id/dishes/:id/edit"
+          element={<EditDishes dishes={dishes} />}
+        />
       </Routes>
     </Router>
   );
