a1384a0bd859b3d84e28749232db1105417c1f56
Nick <nickcorreagarcia@gmail.com>
2022-10-21 19:22:50 -0500

    Add new enpoints update create and delete dishes

--- client/src/services/dishes_services.js
+++ client/src/services/dishes_services.js
@@ -9,4 +9,42 @@ DishesServices.prototype.show = (id) => {
   return fetch(`/restaurants/${id}/dishes`).then((response) => response.json());
 };
 
+DishesServices.prototype.update = (id, name, price, image) => {
+  return fetch(`/dishes/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      name,
+      price,
+      image,
+    }),
+  });
+};
+
+DishesServices.prototype.create = (name, price, image, restaurantId) => {
+  return fetch(`/dishes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      name,
+      price,
+      image,
+      restaurantId,
+    }),
+  });
+};
+
+DishesServices.prototype.delete = (id) => {
+  return fetch(`/dishes/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  });
+};
+
 export default DishesServices;
